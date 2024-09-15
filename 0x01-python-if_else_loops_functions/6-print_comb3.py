e1af38490c469daf6ce721d31f58b6f58233d80a
Anthony <peakybeetle@gmail.com>
2024-09-15 16:20:55 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for m in range(9):
-    for n in range(m + 1, 10):
-        if m != 8:
-            print("{}{}".format(m, n), end=", ")
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i != 8:
+            print("{}{}".format(i, j), end=", ")
         else:
-            print("{}{}".format(n, n))
+            print("{}{}".format(i, j))
